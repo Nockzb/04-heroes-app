f135609556b2bc8568eaf409a9490c66e5da3ca3
Nockzb <132928494+Nockzb@users.noreply.github.com>
2024-02-06 20:18:21 +0100

    auth canactivate canmatch

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 
 import { Observable, catchError, map, of, tap } from 'rxjs';
 import { environments } from '../../../environments/environments';
-import { User } from '../interfaces/auth.interface';
+import { User } from '../interfaces/user.interface';
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
@@ -25,7 +25,26 @@ export class AuthService {
         return this.httpClient.get<User>(`${ this.baseUrl }/users/1`)
             .pipe(
                 tap( user => this.user = user ),
-                tap( user => localStorage.setItem('token', user.id) )
+                tap( user => localStorage.setItem('token', 'giiunasu.ton21lta21512.521asf') )
             );
+    }
+
+    logout() {
+        this.user = undefined;
+        localStorage.clear();
+    }
+
+    checkAuthentication(): Observable<boolean> | boolean {
+        // Si no hay token se devuelve false
+        if (!localStorage.getItem('token')) return false;
+
+        const TOKEN = localStorage.getItem('token');
+
+        return this.httpClient.get<User>(`${ this.baseUrl }/users/1`)
+            .pipe(
+                tap ( user => this.user = user), // almacenamos el usuario en la propiedad
+                map ( user => !!user ), // es lo mismo que "map ( user => user? true : false)", si hay user se devuelve true, sino false
+                catchError ( err => of(false))
+            )
     }
 }
